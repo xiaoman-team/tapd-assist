71069eea2f0ccee720c3443c8500c312c7cd60bd
asinbow <at20042004@gmail.com>
2017-02-15 10:39:27 +0800

    enhance script publish script

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 CURRENT_DIR=`pwd`
-SCRIPTS_DIR=`dirname "${BASH_SOURCE[0]}"`
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $SCRIPTS_DIR/../src \
+    && rm -rf ../src.zip \
     && zip -r ../src.zip . \
     && cd $CURRENT_DIR
 
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-SCRIPTS_DIR=`dirname "${BASH_SOURCE[0]}"`
-CURL_OPTS="--socks5-hostname localhost:2080"
+# your extension id, modify it if needed
+EXTENSION_ID=ifnplghlomamhddgdknfcennkpcjcoke
 
+if [ "$CURL_OPTS" = "" ]; then
+    echo '\033[0;31m[Warn]\033[0m \033[1;33mThis script will access google services. You can config CURL_OPTS to fuck GFW\033[0m'
+    echo '    CURL_OPTS="--proxy=http://localhost:8000/" ./scripts/publish.sh'
+    echo ' or CURL_OPTS="--socks5-hostname localhost:2080" ./scripts/publish.sh'
+fi
+
+# CURL_OPTS=
+
+# token got from https://console.developers.google.com/apis/credentials?project=xiaomanassistextension&authuser=1
 CLIENT_ID=941813370135-unca1mt7pch2nje1j8qfpk83344sv8dn.apps.googleusercontent.com
 CLIENT_SECRET=EcKyG5WpHsIQmGx_Tucx8lxL
-
 REFRESH_TOKEN=1/Lc9krxfpGzQm6yUu2X5UdpPpU_T2qmLXzbMQ9BYrUxmh6eBvExlAECyVPYyycCs4
 
-EXTENSION_ID=ifnplghlomamhddgdknfcennkpcjcoke
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CHROME_LEASE_VERSION=31.0.1609.0
+CRX_FILENAME=$SCRIPTS_DIR/../${EXTENSION_ID}_main.crx
+
+get_manifest_version() {
+    node $SCRIPTS_DIR/json.js version $SCRIPTS_DIR/../src/manifest.json
+}
 
 get_access_token() {
     curl \
@@ -71,19 +85,47 @@ publish_extension() {
     echo $status
 }
 
-echo "packaging zip..." && \
-    $SCRIPTS_DIR/package.sh && \
-    echo "getting access token..." && \
-    ACCESS_TOKEN=`get_access_token` && \
-    echo "access token: $ACCESS_TOKEN" && \
-    echo "uploading zip..." && \
-    upload_extension_zip && \
-    echo "uploaded zip" && \
-    echo "publish extension..." && \
-    publish_extension && \
-    echo "published extension" && \
-    EXTENSION_VERSION=`get_extension_version` && \
-    echo "current version $EXTENSION_VERSION" && \
-    echo "Done"
+get_crx_download_url() {
+    curl \
+        $CURL_OPTS \
+        -s \
+        -D - \
+        "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=$CHROME_LEASE_VERSION&x=id%3D$EXTENSION_ID%26uc" | \
+        grep '^Location:' | \
+        sed 's/^Location:[ \t]*\(.*\)$/\1/'
+}
+
+download_bin() {
+    url=$1
+    filename=$2
+
+    curl \
+        $CURL_OPTS \
+        -s \
+        $url \
+        > $filename
+}
+
+echo "Start" \
+    && MANIFEST_VERSION=`get_manifest_version` \
+    && echo "manifest version $MANIFEST_VERSION" \
+    && echo "packaging zip..." \
+    && $SCRIPTS_DIR/package.sh \
+    && echo "getting access token..." \
+    && ACCESS_TOKEN=`get_access_token` \
+    && echo "access token: $ACCESS_TOKEN" \
+    && echo "uploading zip..." \
+    && upload_extension_zip \
+    && echo "uploaded zip" \
+    && echo "publish extension..." \
+    && publish_extension \
+    && echo "published extension" \
+    && EXTENSION_VERSION=`get_extension_version` \
+    && echo "current chrome store version $EXTENSION_VERSION" \
+    && CRX_DOWNLOAD_URL=`get_crx_download_url` \
+    && echo "download extension $CRX_DOWNLOAD_URL" \
+    && download_bin $CRX_DOWNLOAD_URL $CRX_FILENAME \
+    && echo "downloaded crx $CRX_FILENAME" \
+    && echo "Done"
 
 
