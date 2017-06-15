072ace24226816e912ae77bf266b36e838d92fac
hao <hao@xiaoman.cn>
2017-06-15 11:15:31 +0800

    remove log

--- src/page_inject.js
+++ src/page_inject.js
@@ -7,7 +7,6 @@ $(function (e) {
     if (selection.type !== 'Range') {
       $('#title-copy-btn').click();
       window.TFL.tips.showFlash('【小满助手】标题与链接已复制到剪贴板中')
-      console.log('xxxxxxx bbb', e)
     }
   });
 });
